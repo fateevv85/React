cdec096fd27d32f6f6225d5fe05f0926df3339b9
Vasiliy <fateevv85@gmail.com>
2022-05-22 02:00:37 +0300

    init

---
+++ src/Controller/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test')]
+    public function __invoke():Response
+    {
+        return new Response('Hell112o');
+    }
+}
