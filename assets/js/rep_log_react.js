808d54862889f0b49980541de374a603e20be17e
Vasiliy <fateevv85@gmail.com>
2022-05-22 21:11:28 +0300

    complete 2 lesson add monolog

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,12 @@
-console.log('Oh hallo React peeps! ?️');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const el = <h2>
+    Lift Stuff!
+    <span>'❤️'</span>
+    <span>'another one️'</span>
+</h2>
+
+createRoot(document.getElementById('lift-stuff-app')).render(el);
+
+console.log(el);
--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
