d7e62fc06607aa96d7ff84adf0a657b8792b68fe
Vasiliy <fateevv85@gmail.com>
2022-06-04 14:25:00 +0300

    add repLogTable presentation component

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import RepLogList from "./RepLogList";
+import RepLogTable from "./RepLogTable";
 
 export default class RepLogApp extends Component {
     constructor(props, context) {
@@ -17,76 +17,13 @@ export default class RepLogApp extends Component {
     }
 
     render() {
-        const heart = this.props.withHeart ? <span>❤️</span> : '';
-        let {highlightedRowId} = this.state;
+        const {highlightedRowId} = this.state;
+        const {withHeart} = this.props;
 
-        return (
-            <div className="col-md-7 js-rep-log-table">
-                <h2>Lift Stuff! {heart}</h2>
-
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>What</th>
-                        <th>How many times?</th>
-                        <th>Weight</th>
-                        <th>Total</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <RepLogList highlightedRowId={highlightedRowId} onRowClick={this.handleRowClick}/>
-                    </tbody>
-                    <tfoot>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>&nbsp;</td>
-                        <th>Total</th>
-                        <th>TODO</th>
-                        <td>&nbsp;</td>
-                    </tr>
-                    </tfoot>
-                </table>
-
-                <form className="form-inline">
-                    <div className="form-group">
-                        <label className="sr-only control-label required"
-                               htmlFor="rep_log_item">
-                            What did you lift?
-                        </label>
-                        <select id="rep_log_item"
-                                name="item"
-                                required="required"
-                                className="form-control">
-                            <option value="">What did you
-                                lift?
-                            </option>
-                            <option value="cat">Cat</option>
-                            <option value="fat_cat">Big Fat Cat</option>
-                            <option value="laptop">My Laptop</option>
-                            <option value="coffee_cup">Coffee Cup</option>
-                        </select>
-                    </div>
-                    {' '}
-                    <div className="form-group">
-                        <label className="sr-only control-label required"
-                               htmlFor="rep_log_reps">
-                            How many times?
-                        </label>
-                        <input type="number" id="rep_log_reps"
-                               name="reps" required="required"
-                               placeholder="How many times?"
-                               className="form-control"/>
-                    </div>
-                    {' '}
-                    <div>
-                        <button type="submit"
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            I Lifted it!
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
+        return (<RepLogTable
+            withHeart={withHeart}
+            highlightedRowId={highlightedRowId}
+            handleRowClick={this.handleRowClick}
+        />);
     }
 }
